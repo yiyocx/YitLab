2942b510a395e2db7cffb9346a529b81378fbade
Yiyo Castillo <yiyocx@gmail.com>
2015-08-13 00:14:36 -0500

    Agregada CardView que contendra información de cada uno de los proyectos

--- app/src/main/kotlin/yiyo/gitlabandroid/views/adapters/ProjectsAdapter.kt
+++ app/src/main/kotlin/yiyo/gitlabandroid/views/adapters/ProjectsAdapter.kt
@@ -2,20 +2,23 @@ package yiyo.gitlabandroid.views.adapters
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import yiyo.gitlabandroid.R
 import yiyo.gitlabandroid.model.entities.Project
 
 /**
  * Created by yiyo on 12/08/15.
  */
-class ProjectsAdapter(val projects: List<Project>, val mContext: Context) : RecyclerView.Adapter<ProjectsAdapter.ViewHolder>() {
+class ProjectsAdapter(val projects: List<Project>, val context: Context) : RecyclerView.Adapter<ProjectsAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder? {
-        throw UnsupportedOperationException()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val rootView = LayoutInflater.from(context).inflate(R.layout.card_project_layout, parent, false)
+        return ViewHolder(rootView)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         throw UnsupportedOperationException()
     }
 
